88b81a188e925c9e6df2d5377053b4fd95529747
João Marcos <50593874+joaobem@users.noreply.github.com>
2019-05-13 16:10:23 -0300

    dados completo

--- Python/dados.py
+++ Python/dados.py
@@ -4,16 +4,26 @@ print("Jogo De Dados")
 
 dados1 = randint(1, 6)
 dados2 = randint(1, 6)
+jogador1 = dados1 + dados2
+
 dados3 = randint(1, 6)
 dados4 = randint(1, 6)
+jogador2 = dados3 + dados4
 
 print("Dado 1:", dados1)
 print("Dado 2:", dados2)
-
-+jogador1= dados1+dados2
-
-
 print("Dado 3:", dados3)
 print("Dado 4:", dados4)
 
-+jogador2=dados3+dados4
+
+print("Resultado do jogador 1:", jogador1)
+print("Resultado do jogador 2:", jogador2)
+
+if jogador1 > jogador2:
+    print("Jogador 1 Venceu")
+
+elif jogador2 > jogador1:
+        print("Jogador 2 Venceu")
+    
+else:
+     print("Jogadores Empataram")
