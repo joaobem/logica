248366776443728971be4fc9d6af0746ccaefa91
João Marcos <50593874+joaobem@users.noreply.github.com>
2019-05-13 15:38:42 -0300

    dado 0.2
    cáculo

--- Python/dados.py
+++ Python/dados.py
@@ -9,5 +9,11 @@ dados4 = randint(1, 6)
 
 print("Dado 1:", dados1)
 print("Dado 2:", dados2)
+
++jogador1= dados1+dados2
+
+
 print("Dado 3:", dados3)
 print("Dado 4:", dados4)
+
++jogador2=dados3+dados4
